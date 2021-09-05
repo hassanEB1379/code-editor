714551ac635c7dc9a0a02bbb2943cee438108ec9
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-09-05 13:24:09 +0430

    create button component

---
+++ src/Button.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+   background-color: #444857;
+   padding: 0.8rem 1.2rem;
+   color: #fff;
+   border: none;
+   border-radius: 0.3rem;
+   cursor: pointer;
+`;
+
+export default Button;
