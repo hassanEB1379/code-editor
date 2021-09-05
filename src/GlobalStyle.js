fedbb6643b7d03753217c14b8da1f3d99a7e7e52
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-09-05 13:27:33 +0430

    update global style

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,10 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  :root{
+    --dark-bg : #060606;
+    --dark-border : #2f2f2f;
+  }
+  
   html {
     box-sizing: border-box;
     font-size: calc(60% + 0.8vmin);
-    font-family: 'Open Sans', serif;
+    font-family: 'Open Sans',sans-serif;
+    letter-spacing: 1px;
   }
 
   *, *:after, *:before {
@@ -12,6 +18,7 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
     font-size: 1rem;
+    letter-spacing: inherit;
   }
 `;
 
