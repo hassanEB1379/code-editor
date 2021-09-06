7c170ad8532622935d44b4b3e12df89fa9e1aac5
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-09-06 21:10:02 +0430

    create OutputView component

---
+++ src/OutputView.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+const OutputWrapper = styled.div`
+   flex-grow: 1;
+   width: 1px;
+   position: relative;
+`;
+
+const OutputOverlay = styled.div`
+   position: absolute;
+   inset: 0;
+   z-index: 10;
+`;
+
+const OutputView = () => {
+   return (
+      <OutputWrapper>
+         <OutputOverlay />
+
+         <iframe
+            style={{ backgroundColor: '#fff', border: 'none' }}
+            width="100%"
+            height="100%"
+         />
+      </OutputWrapper>
+   );
+};
+
+export default OutputView;
