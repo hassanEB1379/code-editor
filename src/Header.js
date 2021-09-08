9ef56a220f4ce8dd738e15a0b8ccd9d2217fff7e
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-09-08 22:31:16 +0430

    update header component

--- src/Header.js
+++ src/Header.js
@@ -1,28 +1,55 @@
 import styled from 'styled-components';
-import Button from './Button';
-import Group from './Group';
 
-const HeaderWrapper = styled.div`
+import Button from './ui/Button';
+import Flex from './ui/Flex';
+import ButtonGroup from './ui/ButtonGroup';
+
+// icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ViewIcon } from './icons/ViewIcon';
+import {
+   faChevronDown,
+   faCloud,
+   faCog,
+   faHeart,
+} from '@fortawesome/free-solid-svg-icons';
+
+const HeaderWrapper = styled(Flex)`
    height: 4rem;
-   padding: 0.5rem 1rem;
-   display: flex;
-   align-items: center;
-   justify-content: space-between;
-   background-color: var(--dark-bg);
+   padding: 0.5rem;
    border-bottom: 1px solid var(--dark-border);
 `;
 
 const Header = () => {
    return (
-      <HeaderWrapper>
-         <Button>hi , i am Button</Button>
+      <HeaderWrapper alignItems="center" justifyContent="space-between">
+         <p>sallam i am hassan</p>
 
-         <Group spaceX={0.5}>
-            <Button>Settings</Button>
-            <Button>Settings</Button>
-            <Button>Settings</Button>
-            <Button>Settings</Button>
-         </Group>
+         <Flex gap=".7rem">
+            <Button>
+               <FontAwesomeIcon icon={faHeart} />
+            </Button>
+
+            <ButtonGroup>
+               <Button>
+                  <FontAwesomeIcon icon={faCloud} />
+                  Save
+               </Button>
+
+               <Button dense>
+                  <FontAwesomeIcon icon={faChevronDown} />
+               </Button>
+            </ButtonGroup>
+
+            <Button>
+               <FontAwesomeIcon icon={faCog} />
+               Settings
+            </Button>
+
+            <Button>
+               <ViewIcon />
+            </Button>
+         </Flex>
       </HeaderWrapper>
    );
 };
