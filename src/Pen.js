1b538363db7b40478c994c55fc1735420866727e
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-09-08 22:30:13 +0430

    integrate pen and pen layout

--- src/Pen.js
+++ src/Pen.js
@@ -1,7 +1,47 @@
-import PenLayout from './PenLayout';
+import styled from 'styled-components';
+import Header from './Header';
+import Flex from './ui/Flex';
+import Footer from './Footer';
+import Editor from './Editor';
+import OutputView from './OutputView';
+import Resizable from './ui/Resizable';
+
+const MainWrapper = styled(Flex).attrs(() => ({
+   direction: 'column',
+   justifyContent: 'space-between',
+}))`
+   min-height: 100vh;
+`;
+
+const ContentWrapper = styled(Flex)`
+   flex-grow: 1;
+   border-bottom: 1px solid var(--dark-border);
+`;
+
+const EditorsWrapper = styled(Flex).attrs(() => ({ direction: 'column' }))`
+   border-right: 1px solid var(--dark-border);
+`;
 
 const Pen = () => {
-   return <PenLayout></PenLayout>;
+   return (
+      <MainWrapper>
+         <Header />
+
+         <ContentWrapper>
+            <Resizable initialWidth={500}>
+               <EditorsWrapper>
+                  <Editor mode="html" />
+                  <Editor mode="css" />
+                  <Editor mode="javascript" />
+               </EditorsWrapper>
+            </Resizable>
+
+            <OutputView />
+         </ContentWrapper>
+
+         <Footer />
+      </MainWrapper>
+   );
 };
 
 export default Pen;
