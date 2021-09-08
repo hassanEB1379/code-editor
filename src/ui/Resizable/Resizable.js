72514da1341e279318ba6177aee9af81760c99cb
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-09-08 23:01:18 +0430

    change directory of Resizable component

--- src/ui/Resizable/Resizable.js
+++ src/ui/Resizable/Resizable.js
@@ -1,24 +1,14 @@
-import styled from 'styled-components';
 import { useEffect, useState } from 'react';
+import { ResizableWrapper, ResizerX, ShowSize } from './Resizable.styled';
 
-const ResizableWrapper = styled.div`
-   position: relative;
-`;
-
-const ResizerX = styled.div`
-   cursor: col-resize;
-   width: 1rem;
-   position: absolute;
-   right: 0;
-   height: 100%;
-   z-index: 10;
-   background-color: var(--dark-bg);
-   border-left: 1px solid var(--dark-border);
-`;
-
-const Resizable = ({ children, initialWidth = 100 }) => {
+const Resizable = ({
+   children,
+   initialWidth = 100,
+   enableShowSize = false,
+}) => {
    const [width, setWidth] = useState(initialWidth);
    const [resize, setResize] = useState(false);
+   const [showSize, setShowSize] = useState(false);
 
    const resizeStart = () => setResize(true);
    const resizeEnd = () => setResize(false);
@@ -26,6 +16,8 @@ const Resizable = ({ children, initialWidth = 100 }) => {
    const handleResize = e => {
       if (resize) {
          setWidth(e.clientX);
+
+         if (enableShowSize) setShowSize(true);
       }
    };
 
@@ -39,10 +31,20 @@ const Resizable = ({ children, initialWidth = 100 }) => {
       };
    });
 
+   // hidden ShowSize component after a few seconds
+   useEffect(() => {
+      const timer = setTimeout(() => setShowSize(false), 2000);
+
+      return () => clearTimeout(timer);
+   }, [width]);
+
    return (
       <ResizableWrapper style={{ width }}>
          <ResizerX onMouseDown={resizeStart} onMouseUp={resizeEnd} />
+
          {children}
+
+         {enableShowSize && <ShowSize hidden={!showSize}>{width}px</ShowSize>}
       </ResizableWrapper>
    );
 };
