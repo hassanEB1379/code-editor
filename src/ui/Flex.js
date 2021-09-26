652ced0e27b873ccfef462d9840d1f2876ae76ac
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-09-26 18:32:59 +0330

    update flex component

--- src/ui/Flex.js
+++ src/ui/Flex.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Flex = styled.div`
    display: ${props => (props.inline ? 'inline-flex' : 'flex')};
-   flex-direction: ${props => (props.direction ? props.direction : 'row')};
+   flex-direction: ${props => props.direction ?? 'row'};
    align-items: ${props => props.alignItems};
    justify-content: ${props => props.justifyContent};
    gap: ${props => props.gap};
