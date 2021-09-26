b89f7f24dd018eaafbe1f2a39811d549625acdbc
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-09-26 18:34:18 +0330

    update editor component

--- src/Editor.js
+++ src/Editor.js
@@ -1,26 +1,39 @@
 import styled from 'styled-components';
 import AceEditor from 'react-ace';
+import Flex from './ui/Flex';
 
 import 'ace-builds/src-noconflict/mode-javascript';
 import 'ace-builds/src-noconflict/mode-css';
 import 'ace-builds/src-noconflict/mode-html';
 import 'ace-builds/src-noconflict/theme-twilight';
 
-const EditorWrapper = styled.div`
+const EditorWrapper = styled(Flex).attrs(() => ({ direction: 'column' }))`
+   height: 100%;
+
    &:not(:last-of-type) {
       border-bottom: 1px solid var(--dark-border);
    }
 `;
 
-const Editor = props => {
+const EditorHeader = styled(Flex)`
+   background-color: var(--dark-bg);
+   padding: 0.5rem 1rem;
+`;
+
+const Editor = ({ iconSrc, ...rest }) => {
    return (
       <EditorWrapper>
+         <EditorHeader gap="1rem" alignItems="center">
+            <img alt="lang-icon" width={20} src={iconSrc} />
+            <h4>{rest.mode.toUpperCase()}</h4>
+         </EditorHeader>
+
          <AceEditor
             theme="twilight"
             fontSize="1.2rem"
             width="100%"
-            height="200px"
-            {...props}
+            height="100%"
+            {...rest}
          />
       </EditorWrapper>
    );
