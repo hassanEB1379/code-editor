7f6e14bb576ce9d25e2d1525819b86df8b35efba
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-09-27 18:53:33 +0330

    handle toggle console button

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,21 @@
 import styled from 'styled-components';
 import Flex from './ui/Flex';
 import Button from './ui/Button';
+import { useToggleConsole } from './context/Console.context';
 
 const FooterWrapper = styled(Flex)`
    padding: 0.3rem;
+   border-top: 1px solid var(--dark-border);
 `;
 
 const Footer = () => {
+   const { toggle } = useToggleConsole();
+
    return (
       <FooterWrapper>
-         <Button sm>Console</Button>
+         <Button sm onClick={toggle}>
+            Console
+         </Button>
       </FooterWrapper>
    );
 };
