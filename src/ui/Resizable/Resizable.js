e9f3fabb3e3172927aaa2781d58789bf3f123e5f
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-09-27 18:51:58 +0330

    update resizable component

--- src/ui/Resizable/Resizable.js
+++ src/ui/Resizable/Resizable.js
@@ -1,32 +1,32 @@
 import { ResizableWrapper } from './Resizable.styled';
-import { Children, useMemo } from 'react';
+import { Children, Fragment, useMemo } from 'react';
 import { Resizer } from './Resizer';
 
-const Resizable = ({ children, orientation = 'horizontal', minSize }) => {
+const Resizable = ({ children, orientation = 'horizontal', minSize = 20 }) => {
+   const validElements = Children.toArray(children);
+
    // calculate initial size
    const initialSize = useMemo(() => {
-      const coefficient = 1 / children.length;
+      const coefficient = 1 / validElements.length;
 
       const dimension = orientation === 'horizontal' ? 'width' : 'height';
 
       return {
          [dimension]: `calc((100% - 0px) * ${coefficient})`,
       };
-   }, [children.length]);
+   }, [validElements]);
 
    return (
-      <ResizableWrapper direction={orientation === 'vertical' && 'column'}>
-         {Children.map(children, (child, index) => {
-            return (
-               <>
-                  <div style={{ ...initialSize }}>{child}</div>
+      <ResizableWrapper flexDir={orientation === 'vertical' && 'column'}>
+         {validElements.map((element, index) => (
+            <Fragment key={index}>
+               <div style={{ ...initialSize }}>{element}</div>
 
-                  {index !== children.length - 1 && (
-                     <Resizer orientation={orientation} minSize={minSize} />
-                  )}
-               </>
-            );
-         })}
+               {index !== validElements.length - 1 && (
+                  <Resizer orientation={orientation} minSize={minSize} />
+               )}
+            </Fragment>
+         ))}
       </ResizableWrapper>
    );
 };
