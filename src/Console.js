2e1f18bdc2185742783dfede4b22a740290bc3c1
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-09-27 18:55:05 +0330

    add toggle to close btn

--- src/Console.js
+++ src/Console.js
@@ -5,6 +5,7 @@ import Button from './ui/Button';
 // icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { useToggleConsole } from './context/Console.context';
 
 const ConsoleTitle = styled.p`
    background-color: var(--tab-bg);
@@ -12,25 +13,30 @@ const ConsoleTitle = styled.p`
    font-size: 0.9rem;
    font-weight: 600;
    color: var(--text-disabled);
+   padding: 0.3rem 1rem 0.6rem;
+   border-top: 0.3rem solid var(--tab-indicator);
 `;
 
 const ConsoleBody = styled(Flex).attrs(() => ({
-   direction: 'column',
+   flexDir: 'column',
 }))`
    background-color: var(--tab-bg);
    flex-grow: 1;
 `;
 
 function Console() {
+   const { toggle } = useToggleConsole();
+
    return (
-      <Flex style={{ height: '100%' }} direction="column">
+      <Flex style={{ height: '100%' }} flexDir="column">
          {/* Console header */}
          <Flex justify="space-between" items="center">
             <ConsoleTitle>Console</ConsoleTitle>
 
-            <Flex inline gap=".3rem">
-               <Button sm>Clear</Button>{' '}
-               <Button sm>
+            <Flex inline gap=".3rem" mr=".5rem">
+               <Button sm>Clear</Button>
+
+               <Button sm onClick={toggle}>
                   <FontAwesomeIcon icon={faTimes} />
                </Button>
             </Flex>
