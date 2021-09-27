4e7f48ed5f8f1323d0e1d5461e412aa11d4ab45f
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-09-27 18:54:02 +0330

    change props name

--- src/ui/Flex.js
+++ src/ui/Flex.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
+import Spacing from './Spacing';
 
-const Flex = styled.div`
+const Flex = styled(Spacing)`
    display: ${props => (props.inline ? 'inline-flex' : 'flex')};
-   flex-direction: ${props => props.direction ?? 'row'};
-   align-items: ${props => props.alignItems};
-   justify-content: ${props => props.justifyContent};
+   flex-direction: ${props => props.flexDir ?? 'row'};
+   align-items: ${props => props.items};
+   justify-content: ${props => props.justify};
    gap: ${props => props.gap};
 `;
 
