e4e628d146e94697a3b1e5d12042aa10733d7927
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-09-27 18:50:24 +0330

    add console context provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import Pen from './Pen';
 
+// context providers
+import { ToggleConsoleProvider } from './context/Console.context';
+
 function App() {
-   return <Pen />;
+   return (
+      <ToggleConsoleProvider>
+         <Pen />
+      </ToggleConsoleProvider>
+   );
 }
 
 export default App;
