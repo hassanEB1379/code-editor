bb37f899c7d90f8c30e7267a37f29827e0ee61b8
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-10-10 12:15:22 +0330

    handle write code

--- src/modules/pen/Pen.js
+++ src/modules/pen/Pen.js
@@ -2,12 +2,13 @@ import styled from 'styled-components';
 import Header from '../../Header';
 import Flex from '../../ui/Flex';
 import Footer from '../../Footer';
-import Editor from '../../Editor';
+import Editor from './Editor';
 import OutputView from './OutputView';
 import Resizable from '../../ui/Resizable/Resizable';
 import Console from './Console';
 import { useToggleConsole } from './Console.context';
 import { useViewLayout } from './ViewLayout.context';
+import { useState } from 'react';
 
 const MainWrapper = styled(Flex).attrs(() => ({
    flexDir: 'column',
@@ -17,6 +18,10 @@ const MainWrapper = styled(Flex).attrs(() => ({
 `;
 
 const Pen = () => {
+   const [html, setHtml] = useState('');
+   const [css, setCss] = useState('');
+   const [js, setJs] = useState('');
+
    const { isOpen } = useToggleConsole();
 
    const { wrapper, editors } = useViewLayout();
@@ -27,18 +32,30 @@ const Pen = () => {
 
          <Resizable orientation={wrapper.orientation} minSize={38}>
             <Resizable orientation={editors.orientation} minSize={38}>
-               <Editor mode="html" iconSrc="static/images/html-5.svg" />
-
-               <Editor mode="css" iconSrc="static/images/css-3.svg" />
+               <Editor
+                  onChange={code => setHtml(code)}
+                  mode="html"
+                  name="html-editor"
+                  iconSrc="static/images/html-5.svg"
+               />
 
                <Editor
+                  onChange={code => setCss(code)}
+                  mode="css"
+                  name="css-editor"
+                  iconSrc="static/images/css-3.svg"
+               />
+
+               <Editor
+                  onChange={code => setJs(code)}
                   mode="javascript"
+                  name="js-editor"
                   iconSrc="static/images/javascript.svg"
                />
             </Resizable>
 
             <Resizable orientation="vertical">
-               <OutputView />
+               <OutputView code={{ html, css, js }} />
                {isOpen && <Console />}
             </Resizable>
          </Resizable>
