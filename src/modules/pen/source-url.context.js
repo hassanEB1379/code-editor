7cefb8519071ef09c397eb86b266e3dd615eaf9d
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-10-17 17:35:50 +0330

    create url of iframe context

---
+++ src/modules/pen/source-url.context.js
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from 'react';
+import { getGeneratedPageURL } from './utils/generatePageUrl';
+
+const SourceUrlContext = createContext(null);
+const SourceUrlContextDispatch = createContext(null);
+
+export const useSourceUrl = () => useContext(SourceUrlContext);
+export const useSourceUrlDispatch = () => useContext(SourceUrlContextDispatch);
+
+export function SourceUrlProvider({ children }) {
+   const [url, setUrl] = useState(getGeneratedPageURL());
+
+   return (
+      <SourceUrlContext.Provider value={url}>
+         <SourceUrlContextDispatch.Provider value={setUrl}>
+            {children}
+         </SourceUrlContextDispatch.Provider>
+      </SourceUrlContext.Provider>
+   );
+}
