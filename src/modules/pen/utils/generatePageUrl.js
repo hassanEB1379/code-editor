d5f4bc30c14088ca26cc1c373ce8bacaefec2f81
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-10-17 17:38:04 +0330

    replace code with src of file

--- src/modules/pen/utils/generatePageUrl.js
+++ src/modules/pen/utils/generatePageUrl.js
@@ -1,4 +1,4 @@
-export const getGeneratedPageURL = ({ html, css, js }) => {
+export const getGeneratedPageURL = ({ html = '', css = '', js = '' } = {}) => {
    const getBlobURL = (code, type) => {
       const blob = new Blob([code], { type });
       return URL.createObjectURL(blob);
@@ -13,81 +13,13 @@ export const getGeneratedPageURL = ({ html, css, js }) => {
       <head>
         <title>Output pen</title>
         <meta charset="utf-8">      
-        
-        <script>
-              window.onerror = (message, source, lineno, colno, error) => {
-                   window.parent.postMessage(
-                      {
-                         type: 'error',
-                         source: 'output-view-iframe',
-                         payload: { message, source, lineno, colno, error }
-                      },
-                      '*'
-                   );
-              };
-
-              const customConsole = {
-                 warn: message => {
-                    window.parent.postMessage(
-                       {
-                          type: 'warning',
-                          source: 'output-view-iframe',
-                          payload: { message },
-                       },
-                       '*'
-                    );
-                 },
-                 log: data => {
-                    window.parent.postMessage(
-                       {
-                          type: 'log',
-                          source: 'output-view-iframe',
-                          payload: { data },
-                       },
-                       '*'
-                    );
-                 },
-                 error: message => {
-                    window.parent.postMessage(
-                       {
-                          type: 'error',
-                          source: 'output-view-iframe',
-                          payload: { message },
-                       },
-                       '*'
-                    );
-                 },
-                 clear: () => {
-                    window.parent.postMessage(
-                       {
-                          type: 'clear',
-                          source: 'output-view-iframe',
-                       },
-                       '*'
-                    );
-                 },
-              };
-              
-              window.console = { ...window.console, ...customConsole };
-              
-              // handle command line
-              
-              function runCommand(text) {
-                 try {
-                   const returnedValue = Function('"use strict";return (' + text + ')')();
-                   
-                   console.log(returnedValue)
-                 }catch(err) {
-                   console.error(err.message)
-                 }
-              }
-        </script>
+        <script src='http://localhost:3000/static/libs/handle-iframe-console.js'></script>
         
         ${css && `<link rel="stylesheet" type="text/css" href="${cssURL}" />`}
         ${js && `<script src="${jsURL}"></script>`}    
       </head>
-      <body>
-        ${html || ''}
+      <body >
+        ${html}
       </body>
     </html>
   `;
