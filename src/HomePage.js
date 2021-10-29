774d8d78e960586e61d704a8d05cc7a01f24de0c
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-10-29 16:33:54 +0330

    refactor Homepage component

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,36 +1,34 @@
 import Flex from './ui/Flex';
 import styled from 'styled-components';
 import Button from './ui/Button';
+import { Link } from 'react-router-dom';
 
 const HomeWrapper = styled(Flex)`
    max-width: 80rem;
    margin: 0 auto;
-   padding-top: 3rem;
-   min-height: 100vh;
-`;
-
-const HomeGradient = styled.div`
-   background-image: linear-gradient(0deg, #141e30 0%, #243b55 100%);
 `;
 
 function HomePage() {
    return (
-      <HomeGradient>
-         <HomeWrapper flexDir="column" gap="1.5rem">
-            <Flex items="center" justify="space-between">
-               <Flex flexDir="column" inline>
-                  <h1>Online code editor</h1>
-                  <p>online code editor for web development</p>
-               </Flex>
-
-               <Flex gap="1rem">
-                  <Button>Login</Button>
-                  <Button>Register</Button>
-               </Flex>
+      <HomeWrapper pt="3rem" flexDir="column" gap="1.5rem">
+         <Flex items="center" justify="space-between">
+            <Flex flexDir="column" inline>
+               <h1>Online code editor</h1>
+               <p>online code editor for web development</p>
             </Flex>
-            <img alt="home-page-img" src="/static/images/home-img.png" />
-         </HomeWrapper>
-      </HomeGradient>
+
+            <Flex gap="1rem">
+               <Link to="/login">
+                  <Button>Login</Button>
+               </Link>
+
+               <Link to="/register">
+                  <Button>Register</Button>
+               </Link>
+            </Flex>
+         </Flex>
+         <img alt="home-page-img" src="/static/images/home-img.png" />
+      </HomeWrapper>
    );
 }
 
