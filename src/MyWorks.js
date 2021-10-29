06b7917a020086275a0ce6579c5cf736813db88b
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-10-29 19:13:42 +0330

    create MyWorks component

---
+++ src/MyWorks.js
@@ -0,0 +1,12 @@
+import { Container, Divider } from './ui';
+
+function MyWorks() {
+   return (
+      <Container maxWidth="70rem" pt="4rem" px="1rem">
+         <h1>My works</h1>
+         <Divider />
+      </Container>
+   );
+}
+
+export default MyWorks;
