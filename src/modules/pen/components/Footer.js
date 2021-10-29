b6a02a536a2c0832850aec97a70cfa62d2c27211
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-10-29 16:38:12 +0330

    add logout button to footer

--- src/modules/pen/components/Footer.js
+++ src/modules/pen/components/Footer.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import Flex from '../../../ui/Flex';
 import Button from '../../../ui/Button';
 import { useToggleConsole } from '../console/ConsoleToggle.context';
+import { useLogout } from '../../authentication/hooks/useLogout';
 
 const FooterWrapper = styled(Flex)`
    padding: 0.3rem;
@@ -11,10 +12,15 @@ const FooterWrapper = styled(Flex)`
 const Footer = () => {
    const { toggle } = useToggleConsole();
 
+   const logout = useLogout();
+
    return (
-      <FooterWrapper>
+      <FooterWrapper justify="space-between">
          <Button sm onClick={toggle}>
             Console
+         </Button>
+         <Button sm onClick={logout}>
+            Logout
          </Button>
       </FooterWrapper>
    );
