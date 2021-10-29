7da02551eca2ce23241c899fb5d6f7368256a318
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-10-29 16:32:18 +0330

    add forms variable to global style

--- src/ui/GlobalStyle.js
+++ src/ui/GlobalStyle.js
@@ -3,24 +3,30 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   :root {
     --dark-bg: #060606;
+    --dark-gradient: linear-gradient(0deg, #141e30 0%, #243b55 100%);
     --dark-btn: #444857;
     --dark-bth-hover: #5a5f73;
     --dark-border: #2f2f2f;
     --contrast-text: #fff;
-    --text-disabled : #aaaebc;
+    --text-disabled: #aaaebc;
     --tab-bg: #1d1e22;
-    --tab-indicator : #34363e;
+    --tab-indicator: #34363e;
     --dropdown-bg: #1e1f26;
     --command-line-bg: #ffffff2e;
     --command-line-return-bg: #ffffff21;
     --divider-bg: #4b4b4b;
-    --z-999 : 999;
-    --z-100 : 100;
-    --z-50 : 50;
+    --z-999: 999;
+    --z-100: 100;
+    --z-50: 50;
     --z-30: 30;
     --z-10: 10;
+    --form-border: #223851;
+    --form-submit-bg: #26b026;
+    --field-border: #cccccc;
+    --field-border-focus: #b190ff;
+    --form-error-text: #ff4949;
   }
-  
+
   html {
     box-sizing: border-box;
     font-size: calc(60% + 0.8vmin);
@@ -29,43 +35,60 @@ const GlobalStyle = createGlobalStyle`
   body {
     color: var(--contrast-text);
     background-color: var(--dark-bg);
+    background-image: var(--dark-gradient);
     font-family: 'Open Sans', sans-serif;
     letter-spacing: 1px;
     user-select: none;
+    min-height: 100vh;
   }
-  
+
   *, *:after, *:before {
     box-sizing: inherit;
     padding: 0;
     margin: 0;
   }
 
+  ul {
+    list-style: none;
+  }
+
+  // reset link style
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  // reset input default style
   button, input, select, textarea {
     font-family: inherit;
     font-size: 100%;
     letter-spacing: inherit;
   }
-  
-  // reset input default style
+
   input {
     appearance: none;
-    border:none;
+    border: none;
+
     &:focus {
-      outline : none;
+      outline: none;
     }
   }
-  
+
+  button {
+    color: inherit;
+  }
+
   // create custom title tooltip (using data-title attribute)
   [data-title] {
-    position:relative;
+    position: relative;
   }
-  
+
   [data-title]:hover:after {
     opacity: 1;
     transition: all 0.1s ease 0.3s;
     visibility: visible;
   }
-  
+
   [data-title]:after {
     content: attr(data-title);
     font-size: .75rem;
