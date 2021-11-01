d7937aed6e837721bcb16381868296443703a35f
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-11-01 11:54:39 +0330

    create 404 page

---
+++ src/components/404.js
@@ -0,0 +1,12 @@
+import { Flex } from '../ui';
+
+function Page404() {
+   return (
+      <Flex flexDir="column" items="center" justify="center">
+         <h1>404</h1>
+         <h2>Page not found</h2>
+      </Flex>
+   );
+}
+
+export default Page404;
