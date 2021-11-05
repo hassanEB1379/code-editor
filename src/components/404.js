89aeb3f6b44164604594e4e1007448c35e3ddfb3
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-11-05 12:58:24 +0330

    create 404 page

--- src/components/404.js
+++ src/components/404.js
@@ -1,10 +1,14 @@
-import { Flex } from '../ui';
+import { Button, Flex } from '../ui';
+import { Link } from 'react-router-dom';
 
 function Page404() {
    return (
-      <Flex flexDir="column" items="center" justify="center">
+      <Flex gap="1.5rem" flexDir="column" items="center" justify="center">
          <h1>404</h1>
          <h2>Page not found</h2>
+         <Link to="/">
+            <Button>Go home</Button>
+         </Link>
       </Flex>
    );
 }
