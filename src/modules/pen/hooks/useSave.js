bf82d2205e4c6cf9c6372e020fc6c1090bc8b192
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-11-05 13:14:04 +0330

    save new pen

--- src/modules/pen/hooks/useSave.js
+++ src/modules/pen/hooks/useSave.js
@@ -4,6 +4,17 @@ export function useSave() {
    const pen = usePen();
 
    return function () {
-      localStorage.setItem('pen', JSON.stringify(pen));
+      try {
+         // get previous items and remove old pen
+         let prevItems = JSON.parse(localStorage.getItem('pens'));
+         let newItems = prevItems.filter(prevItem => prevItem.id !== pen.id);
+
+         // push new pen to items
+         newItems.push(pen);
+         // store new items
+         localStorage.setItem('pens', JSON.stringify(newItems));
+      } catch (err) {
+         console.log(err);
+      }
    };
 }
