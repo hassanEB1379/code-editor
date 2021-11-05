582f15912ecb502e53c0f40a065b7a1792dff249
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-11-05 13:00:04 +0330

    add 404 page and add exact prop to routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './HomePage';
 import Pen from '../modules/pen/components/Pen';
 import {
@@ -8,16 +8,25 @@ import {
    Register,
 } from '../modules/authentication/components';
 import MyWorks from './MyWorks';
+import Page404 from './404';
 
 function App() {
    return (
       <BrowserRouter>
          <Switch>
-            <PrivateRoute path="/my-works" component={MyWorks} />
-            <PublicRoute restricted path="/login" component={Login} />
-            <PublicRoute restricted path="/register" component={Register} />
-            <PrivateRoute path="/pen" component={Pen} />
-            <PublicRoute path="/" component={HomePage} />
+            <PrivateRoute exact path="/my-works" component={MyWorks} />
+            <PublicRoute exact restricted path="/login" component={Login} />
+            <PublicRoute
+               exact
+               restricted
+               path="/register"
+               component={Register}
+            />
+            <PrivateRoute exact path="/pen/:id" component={Pen} />
+            <PublicRoute exact path="/" component={HomePage} />
+
+            {/* not found page */}
+            <Route component={Page404} />
          </Switch>
       </BrowserRouter>
    );
