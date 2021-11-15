ff8f381a30becd0bec75ad900d2228c164c78d1a
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-11-15 10:20:52 +0330

    create indexedDB with dexie

---
+++ src/indexedDB.js
@@ -0,0 +1,8 @@
+// we using dexie-js for manage indexedDB
+import Dexie from 'dexie';
+
+// create new database
+export const db = new Dexie('codeEditor');
+
+// object stores
+export const stores = { pens: 'id,title,image,code' };
