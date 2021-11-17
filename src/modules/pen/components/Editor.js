19e61ac333dc25860701c3dfb439d5908b9fca20
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-11-17 11:13:17 +0330

    update unsaved change in onChange editor and show alert to user

--- src/modules/pen/components/Editor.js
+++ src/modules/pen/components/Editor.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 import AceEditor from 'react-ace';
 import { Flex } from '../../../ui';
+import {
+   useUnsavedChangesCount,
+   useUnsavedChangesDispatch,
+} from '../contexts/unsaved-changes-context';
 import { usePenDispatch } from '../contexts/pen-context';
+import { useCustomAlert } from '../../alerts/useCustomAlert';
 
 import 'ace-builds/src-noconflict/mode-javascript';
 import 'ace-builds/src-noconflict/mode-css';
@@ -26,10 +31,20 @@ const EditorHeader = styled(Flex)`
 `;
 
 const Editor = ({ iconSrc, type, ...rest }) => {
-   const dispatch = usePenDispatch();
+   const unsavedChanges = useUnsavedChangesCount();
+   const PenDispatch = usePenDispatch();
+   const unsavedChangesDispatch = useUnsavedChangesDispatch();
+   const { showWarningAlert } = useCustomAlert();
 
    function handleChangeSource(code) {
-      dispatch({ type, payload: code });
+      // increment unsaved changes
+      unsavedChangesDispatch(prev => prev + 1);
+      // show warning alert for saving
+      if (unsavedChanges % 50 === 0 && unsavedChanges !== 0) {
+         showWarningAlert(`There is ${unsavedChanges} unsaved changes`);
+      }
+      // update pen context with new code
+      PenDispatch({ type, payload: code });
    }
 
    return (
