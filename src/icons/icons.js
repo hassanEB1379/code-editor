893eb1588b4ffabee0085b6a01b10c4260eb57ef
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-12-10 16:21:25 +0330

    export useful icons

---
+++ src/icons/icons.js
@@ -0,0 +1,6 @@
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// This file export useful icons
+
+export const CloseIcon = () => <FontAwesomeIcon icon={faTimes} />;
