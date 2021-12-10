3f6721b9267393879ecd4c8f87409197361942be
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-12-10 16:20:07 +0330

    refactor change view dropdown

---
+++ src/modules/pen/view-layout/ChangeViewDropdown.js
@@ -0,0 +1,35 @@
+import { useViewLayout, useViewLayoutDispatch } from './ViewLayout.context';
+import { Button, ButtonGroup, Dropdown, Spacing } from '../../../ui';
+import ViewLayoutIcon from './ViewLayout.icon';
+import { isDesktop } from 'react-device-detect';
+
+// This component change editors and output layout (vertical or horizontal).
+// This feature available on Desktop
+export function ChangeViewDropdown() {
+   const dispatch = useViewLayoutDispatch();
+
+   const { icon } = useViewLayout();
+
+   function changeLayout(type) {
+      dispatch({ type });
+   }
+
+   return (
+      isDesktop && (
+         <Dropdown action={<Button>{icon}</Button>}>
+            <Spacing className="flex dir-c gap-2" p=".5rem">
+               <h4>Change view</h4>
+               <ButtonGroup>
+                  <Button wide onClick={() => changeLayout('default')}>
+                     <ViewLayoutIcon rotate={-90} />
+                  </Button>
+
+                  <Button wide onClick={() => changeLayout('vertical')}>
+                     <ViewLayoutIcon />
+                  </Button>
+               </ButtonGroup>
+            </Spacing>
+         </Dropdown>
+      )
+   );
+}
