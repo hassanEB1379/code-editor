c675204e6a49304794a6bf3cecdea89c26b5f1a7
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-12-12 20:33:26 +0330

    create Input component

---
+++ src/ui/Input.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+   width: 100%;
+   min-height: 3rem;
+   padding: 0 1rem;
+   border-radius: 0.25rem;
+   background-color: var(--input-bg);
+`;
+
+export const SearchInput = styled(Input)``;
