52694e3cf07b508058ee5a2892cc89dae7816d04
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-12-24 22:00:29 +0330

    refactor view layout sub module

--- src/modules/pen/view-layout/ChangeViewDropdown.js
+++ src/modules/pen/view-layout/ChangeViewDropdown.js
@@ -1,28 +1,29 @@
-import { useViewLayout, useViewLayoutDispatch } from './ViewLayout.context';
+import {
+   horizontal,
+   useViewLayout,
+   useViewLayoutDispatch,
+   vertical,
+} from './ViewLayout-context';
 import { Button, ButtonGroup, Dropdown, Spacing } from '../../../ui';
-import ViewLayoutIcon from './ViewLayout.icon';
+import ViewLayoutIcon from './ViewLayout-icon';
 
 // This component change editors and output layout (vertical or horizontal).
-// This feature available on Desktop
+// This features available on Desktop
 export function ChangeViewDropdown() {
    const dispatch = useViewLayoutDispatch();
 
    const { icon } = useViewLayout();
-
-   function changeLayout(type) {
-      dispatch({ type });
-   }
 
    return (
       <Dropdown action={<Button>{icon}</Button>}>
          <Spacing className="flex dir-c gap-2" p=".5rem">
             <h4>Change view</h4>
             <ButtonGroup>
-               <Button wide onClick={() => changeLayout('default')}>
+               <Button wide onClick={() => dispatch(horizontal())}>
                   <ViewLayoutIcon rotate={-90} />
                </Button>
 
-               <Button wide onClick={() => changeLayout('vertical')}>
+               <Button wide onClick={() => dispatch(vertical())}>
                   <ViewLayoutIcon />
                </Button>
             </ButtonGroup>
--- src/modules/pen/view-layout/ViewLayout.js
+++ src/modules/pen/view-layout/ViewLayout.js
@@ -1,29 +1,38 @@
+import { useEffect } from 'react';
 import { Resizable } from '../../../ui';
 import Output from '../components/Output';
 import Console from '../../console/components/Console';
-import { useViewLayout } from './ViewLayout.context';
 import { useToggleConsole } from '../../console/contexts/ConsoleToggle-context';
 import MobileEditors from '../components/MobileEditors';
 import DesktopEditors from '../components/DesktopEditors';
 import { useToggleOutput } from '../contexts/toggle-output-context';
 import { useDeviceDetect } from '../../../hooks/useDeviceDetect';
+import {
+   useViewLayout,
+   useViewLayoutDispatch,
+   vertical,
+} from './ViewLayout-context';
 
 // This component layout editors and output window responsively
 function ViewLayout() {
    const { isDesktop, isMobile } = useDeviceDetect();
 
    const { wrapper } = useViewLayout();
+   const dispatch = useViewLayoutDispatch();
 
    const { isOpen: consoleOpen } = useToggleConsole();
    const outputOpen = useToggleOutput();
 
+   // vertical editor in mobile
+   useEffect(() => {
+      if (isMobile) dispatch(vertical());
+   }, [isMobile]);
+
    return (
       <Resizable orientation={wrapper.orientation} minSize={200}>
-         {/* Editors section */}
          {isDesktop && <DesktopEditors />}
          {isMobile && <MobileEditors />}
 
-         {/* Output section */}
          {outputOpen && (
             <Resizable orientation="vertical">
                <Output />
