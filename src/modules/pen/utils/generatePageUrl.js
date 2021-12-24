0433b0b778a8a145d82ff9d2f26bc17b98637dc5
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-12-24 22:13:56 +0330

    render added libraries

--- src/modules/pen/utils/generatePageUrl.js
+++ src/modules/pen/utils/generatePageUrl.js
@@ -1,8 +1,7 @@
-export const getGeneratedPageURL = ({
-   html = '',
-   css = '',
-   javascript = '',
-} = {}) => {
+export const getGeneratedPageURL = (
+   { html = '', css = '', javascript = '' } = {},
+   libraries = []
+) => {
    const getBlobURL = (code, type) => {
       const blob = new Blob([code], { type });
       return URL.createObjectURL(blob);
@@ -11,6 +10,21 @@ export const getGeneratedPageURL = ({
    const cssURL = getBlobURL(css, 'text/css');
    const jsURL = getBlobURL(javascript, 'text/javascript');
 
+   // convert library urls to script ( link ) tag then convert library array to string
+   const renderedLibs = libraries
+      .map(library => {
+         let { fileType, latest } = library;
+
+         if (fileType === 'js') {
+            return `<script src="${latest}"></script>`;
+         }
+         if (fileType === 'css') {
+            return `<link rel="stylesheet" href="${latest}"/>`;
+         }
+         return '';
+      })
+      .join(' ');
+
    // create document of output iframe
    const source = `
     <html lang="en">
@@ -18,6 +32,8 @@ export const getGeneratedPageURL = ({
         <title>Output pen</title>
         <meta charset="utf-8">      
         <script src='http://localhost:3000/static/libs/handle-iframe-console.js'></script>
+        
+        ${renderedLibs}
         
         ${
            css && `<link rel="stylesheet" type="text/css" href="${cssURL}" />`
