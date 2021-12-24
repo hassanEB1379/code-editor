b64a2ab7e8c8143b03625288aeb68f23eecef51d
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-12-24 22:14:43 +0330

    refactor Editor

--- src/modules/pen/components/Editor.js
+++ src/modules/pen/components/Editor.js
@@ -1,35 +1,33 @@
 import styled from 'styled-components';
+import { Text } from '../../../ui';
 import AceEditor from 'react-ace';
+import { updateCode, usePenDispatch } from '../contexts/pen-context';
+import { useCustomAlert } from '../../alerts/useCustomAlert';
 import {
    useUnsavedChangesCount,
    useUnsavedChangesDispatch,
 } from '../contexts/unsaved-changes-context';
-import { usePenDispatch } from '../contexts/pen-context';
-import { useCustomAlert } from '../../alerts/useCustomAlert';
 
 import 'ace-builds/src-noconflict/mode-javascript';
 import 'ace-builds/src-noconflict/mode-css';
 import 'ace-builds/src-noconflict/mode-html';
 import 'ace-builds/src-noconflict/theme-twilight';
 
-const EditorWrapper = styled.div.attrs(() => ({ className: 'flex dir-c' }))`
+const EditorWrapper = styled.div`
    height: 100%;
-
+   display: flex;
+   flex-direction: column;
    &:not(:last-of-type) {
       border-bottom: 1px solid var(--dark-border);
    }
 `;
 
-const EditorHeader = styled.div.attrs(() => ({
-   className: 'flex items-center',
-}))`
+const EditorHeader = styled.div`
+   display: flex;
+   align-items: center;
    gap: 1rem;
    background-color: var(--dark-bg);
    padding: 0.5rem 1rem;
-
-   & > h4 {
-      color: var(--text-disabled);
-   }
 `;
 
 const Editor = ({ iconSrc, ...rest }) => {
@@ -46,14 +44,16 @@ const Editor = ({ iconSrc, ...rest }) => {
          showWarningAlert(`There is ${unsavedChanges} unsaved changes`);
       }
       // update pen context with new code
-      PenDispatch({ type: rest.mode, payload: code });
+      PenDispatch(updateCode(rest.mode, code));
    }
 
    return (
       <EditorWrapper>
          <EditorHeader>
             <img alt="lang-icon" width={20} src={iconSrc} />
-            <h4>{rest.mode.toUpperCase()}</h4>
+            <Text textTransform="uppercase" as="h4">
+               {rest.mode === 'javascript' ? 'js' : rest.mode}
+            </Text>
          </EditorHeader>
 
          <AceEditor
