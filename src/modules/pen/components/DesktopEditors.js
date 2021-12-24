482083641a1f3c760fc9dffb07db84a9ef0eaacf
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-12-24 22:15:08 +0330

    using array for render editors

--- src/modules/pen/components/DesktopEditors.js
+++ src/modules/pen/components/DesktopEditors.js
@@ -1,35 +1,28 @@
 import Editor from './Editor';
 import { Resizable } from '../../../ui';
-import { useViewLayout } from '../view-layout/ViewLayout.context';
+import { useViewLayout } from '../view-layout/ViewLayout-context';
 import { usePen } from '../contexts/pen-context';
 
+const editors = [
+   { mode: 'html', name: 'html-editor', iconSrc: '/static/images/html.svg' },
+   { mode: 'css', name: 'css-editor', iconSrc: '/static/images/css.svg' },
+   {
+      mode: 'javascript',
+      name: 'js-editor',
+      iconSrc: '/static/images/javascript.svg',
+   },
+];
+
 function DesktopEditors() {
-   const { editors } = useViewLayout();
+   const { editors: layout } = useViewLayout();
 
    const { code } = usePen();
 
    return (
-      <Resizable orientation={editors.orientation} minSize={38}>
-         <Editor
-            mode="html"
-            name="html-editor"
-            value={code.html}
-            iconSrc="/static/images/html.svg"
-         />
-
-         <Editor
-            mode="css"
-            name="css-editor"
-            value={code.css}
-            iconSrc="/static/images/css.svg"
-         />
-
-         <Editor
-            mode="javascript"
-            name="js-editor"
-            value={code.javascript}
-            iconSrc="/static/images/javascript.svg"
-         />
+      <Resizable orientation={layout.orientation} minSize={38}>
+         {editors.map(editorProps => (
+            <Editor {...editorProps} value={code[editorProps.mode]} />
+         ))}
       </Resizable>
    );
 }
