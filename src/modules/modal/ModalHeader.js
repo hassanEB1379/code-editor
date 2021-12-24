6bfa63c3f70f5b3aa834dc4bb9d930f44ff3db8b
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-12-24 22:09:52 +0330

    add onModalClose event to ModalHeader component

--- src/modules/modal/ModalHeader.js
+++ src/modules/modal/ModalHeader.js
@@ -2,15 +2,20 @@ import { Button, Divider } from '../../ui';
 import { useControlModal } from './ModalProvider';
 import { CloseIcon } from '../../icons/icons';
 
-function ModalHeader({ title }) {
+function ModalHeader({ title, onModalClose }) {
    const { deleteModal } = useControlModal();
+
+   function handleClose() {
+      deleteModal();
+      if (typeof onModalClose === 'function') onModalClose();
+   }
 
    return (
       <>
          <div className="flex justify-between">
             <h2>{title}</h2>
 
-            <Button sm onClick={deleteModal}>
+            <Button sm onClick={handleClose}>
                <CloseIcon />
             </Button>
          </div>
