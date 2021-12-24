b799e63b0a3bb918a81604453382f95e83acb156
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-12-24 22:14:20 +0330

    refactor component

--- src/modules/pen/components/Footer.js
+++ src/modules/pen/components/Footer.js
@@ -1,36 +1,38 @@
 import styled from 'styled-components';
-import { Button, Group, Spacing } from '../../../ui';
+import { Button } from '../../../ui';
 import { useToggleConsole } from '../../console/contexts/ConsoleToggle-context';
 import { useLogout } from '../../authentication/hooks/useLogout';
 import { useControlModal } from '../../modal/ModalProvider';
-import Assets from '../add-assets/Assets';
 
-const FooterWrapper = styled(Spacing).attrs(() => ({
-   className: 'flex justify-between',
-   p: '.3rem',
-}))`
+// components displayed in modal
+import Assets from '../add-assets/Assets';
+import Libraries from '../add-libraries/Libraries';
+
+const FooterWrapper = styled.div`
+   display: flex;
+   justify-content: space-between;
+   padding: 0.3rem;
    border-top: 1px solid var(--dark-border);
 `;
 
 const Footer = () => {
    const { toggle } = useToggleConsole();
    const { showModal } = useControlModal();
-
    const logout = useLogout();
-
-   // Show assets modal component when click on button
-   const handleShowAssets = () => showModal(<Assets />);
 
    return (
       <FooterWrapper>
-         <Group spaceX=".2">
+         <div className="flex gap-1">
             <Button sm onClick={toggle}>
                Console
             </Button>
-            <Button sm onClick={handleShowAssets}>
+            <Button sm onClick={() => showModal(<Assets />)}>
                Assets
             </Button>
-         </Group>
+            <Button sm onClick={() => showModal(<Libraries />)}>
+               Libraries
+            </Button>
+         </div>
 
          <Button sm onClick={logout}>
             Logout
