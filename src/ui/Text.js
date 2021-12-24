88dbad40f2bf068f988eeb0cb412e0619f6eea42
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-12-24 22:03:57 +0330

    add more features to Text component

--- src/ui/Text.js
+++ src/ui/Text.js
@@ -2,9 +2,17 @@ import styled from 'styled-components';
 import { Spacing } from './Spacing';
 
 export const Text = styled(Spacing).attrs(({ as = 'p' }) => ({ as }))`
-   color: ${({ color }) => color};
+   color: ${({ textColor }) => textColor};
    font-size: ${({ size }) => size};
    font-style: ${({ fStyle }) => fStyle};
    font-weight: ${({ weight }) => weight};
    line-height: ${({ lineH }) => lineH};
+   text-transform: ${({ textTransform }) => textTransform};
+   ${({ truncate }) =>
+      truncate &&
+      `
+      overflow : hidden;
+      text-overflow : ellipsis;
+      white-space: nowrap;
+    `}
 `;
