8e19f86d42bec633ec348e5f07b4d089b56be752
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-01-06 13:00:29 +0330

    show dom elements in console

--- src/modules/console/components/StyleByDataType.js
+++ src/modules/console/components/StyleByDataType.js
@@ -1,16 +1,23 @@
 import { FormattedObject } from './FormattedObject';
 import { StyledNumber, StyledString } from '../styled/styled-StyleByDataType';
+import StyledDOMElement from './StyledDOMElement';
 
 export function StyleByDataType({ data }) {
    let dataType = typeof data;
    let isArray = Array.isArray(data);
 
    if (dataType === 'string') {
+      if (data.slice(-10) === '_QEwF5vjI1') {
+         // this is a dom element
+         return <StyledDOMElement element={data.slice(0, -10)} />;
+      }
       return <StyledString>{`'${data}'`}</StyledString>;
    }
+
    if (dataType === 'object') {
       return <FormattedObject object={data} array={isArray} />;
    }
+
    if (
       dataType === 'number' ||
       dataType === 'boolean' ||
