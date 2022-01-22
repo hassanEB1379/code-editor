aa470a154493d26bcaac21d39c79e6f476444709
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-01-22 18:51:12 +0330

    create header for editors

--- src/modules/pen/apprearance/WindowModeEditors.js
+++ src/modules/pen/apprearance/WindowModeEditors.js
@@ -1,12 +1,22 @@
-import { Resizable } from '../../../ui';
+import { Box, Resizable } from '../../../ui';
 import Editor from '../components/Editor';
 import { useState } from '@hookstate/core';
 import { penState } from '../states';
+import styled from 'styled-components';
 
 // icons
 import htmlIcon from '../../../ui/images/html.svg';
 import cssIcon from '../../../ui/images/css.svg';
 import jsIcon from '../../../ui/images/javascript.svg';
+
+const EditorHeader = styled.div`
+   height: 2rem;
+   display: flex;
+   align-items: center;
+   gap: 1.1rem;
+   background-color: var(--primary);
+   padding: 0 1rem;
+`;
 
 const editors = [
    { mode: 'html', name: 'html-editor', icon: htmlIcon },
@@ -24,13 +34,22 @@ export function WindowModeEditors() {
    return (
       <Resizable orientation="vertical" minSize={38}>
          {editors.map((editorProps, index) => (
-            <Editor
-               key={index}
-               options={{
-                  ...editorProps,
-                  value: pen.code.get()[editorProps.mode],
-               }}
-            />
+            <Box className="flex dir-c" key={index}>
+               <EditorHeader>
+                  <img
+                     width="18px"
+                     alt={editorProps.name + '-icon'}
+                     src={editorProps.icon}
+                  />
+                  {editorProps.mode}
+               </EditorHeader>
+               <Editor
+                  options={{
+                     ...editorProps,
+                     value: pen.code.get()[editorProps.mode],
+                  }}
+               />
+            </Box>
          ))}
       </Resizable>
    );
