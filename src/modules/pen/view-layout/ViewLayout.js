46169775afd998ffc24a68f3ff923ef219a57d28
hassanEB1379 <hassanebrahimi560@gmail.com>
2022-01-22 11:52:56 +0330

    apply change to ViewLayout component

--- src/modules/pen/view-layout/ViewLayout.js
+++ src/modules/pen/view-layout/ViewLayout.js
@@ -1,31 +1,25 @@
-import { useEffect } from 'react';
 import { Resizable } from '../../../ui';
 import Output from '../components/Output';
 import Console from '../console/Console';
-import MobileEditors from '../components/MobileEditors';
-import DesktopEditors from '../components/DesktopEditors';
 import { useDeviceDetect } from '../../../hooks/useDeviceDetect';
 import { useState } from '@hookstate/core';
-import { openConsoleState, openOutputState, viewLayoutState } from '../states';
-import { verticalTemplate } from './ViewLayout-templates';
+import { openConsoleState, openOutputState } from '../states';
+import { EditorFactory } from '../apprearance/EditorFactory';
 
 // This component layout editors and output window responsively
 function ViewLayout() {
-   const layout = useState(viewLayoutState);
    const openConsole = useState(openConsoleState);
    const openOutput = useState(openOutputState);
 
    const { isDesktop, isMobile } = useDeviceDetect();
 
-   // vertical editor in mobile
-   useEffect(() => {
-      if (isMobile) layout.set(verticalTemplate);
-   }, [isMobile]);
-
    return (
-      <Resizable orientation={layout.wrapper.orientation.get()} minSize={200}>
-         {isDesktop && <DesktopEditors />}
-         {isMobile && <MobileEditors />}
+      <Resizable
+         orientation={isDesktop ? 'horizontal' : 'vertical'}
+         minSize={200}
+      >
+         {isDesktop && <EditorFactory />}
+         {isMobile && <EditorFactory manual="tab-mode" />}
 
          {openOutput.get() && (
             <Resizable orientation="vertical">
